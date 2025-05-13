7b57726d52d2587c10369d2b695e84ad6dcec212
Roby Lin <95467204+robylin829@users.noreply.github.com>
2025-05-13 14:39:26 +0800

    Add comment

--- core.py
+++ core.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
 
-class Cog_Extension(commands.Cog):
-     def __init__(self,bot):
-        self.bot = bot    
+class Cog_Extension(commands.Cog): # 宣告一個新的 Cog 類別
+     def __init__(self,bot): # 初始化函式，當這個 Cog 被加載時執行
+        self.bot = bot    # 儲存 bot 實例以便之後使用
